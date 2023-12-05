6b31658fc6da0dfbae413961add0ebd1f295e315
Ahmed Sufiyan <ahsu891@gmail.com>
2023-12-05 08:58:22 +0300

    make connect the database cinnect and server start

--- server/index.js
+++ server/index.js
@@ -1,1 +1,28 @@
-import express, { json } from "express";
+import express from "express";
+import { db } from "./db.js";
+
+const app = express();
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/team", teamRoutes);
+// app.use("/api/books", booksRoutes);
+// app.use("/api/webcontent", webContentRoutes);
+
+app.get("/", (req, res) => {
+  res.json("success");
+});
+
+app.listen(8800, () => {
+  console.log("Server running on port 8800...");
+  db.connect((error) => {
+    if (error) {
+      console.error("Error connecting to the database:", error);
+      return;
+    }
+
+    console.log("Connected to the database.");
+  });
+});
